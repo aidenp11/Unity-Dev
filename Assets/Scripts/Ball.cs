7bcab18d1288053498d488eb1048c6adae3800bd
aidenp11 <129340716+aidenp11@users.noreply.github.com>
2024-01-04 15:22:00 -0700

    epilipesy simulator

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField][Range(1, 20)][Tooltip("Force to move object.")] float force;
+
+    public Rigidbody rb;
+    private void Awake()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(transform.up * force, ForceMode.VelocityChange);
+        }
     }
 }
