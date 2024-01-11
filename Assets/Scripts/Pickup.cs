d15f7c01cd611002658d0bb11e863fbb46579723
aidenp11 <129340716+aidenp11@users.noreply.github.com>
2024-01-11 15:24:04 -0700

    animator stuff cool stuffs

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -6,16 +6,6 @@ public class Pickup : MonoBehaviour
 {
     [SerializeField] GameObject pickupPrefab = null;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -24,6 +14,11 @@ public class Pickup : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.TryGetComponent<Player>(out Player player))
+        {
+            player.AddPoint(10);
+        }
+
         Instantiate(pickupPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
